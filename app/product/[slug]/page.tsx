9211b6673e0113ee704f1a0ecf5c1adb3c4a3b0f
vellfar <vellfarenterprises@gmail.com>
2025-01-04 10:41:14 +0300

    slug

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -2,13 +2,8 @@ import { Suspense } from 'react';
 import { createClient } from '@sanity/client';
 import ProductDetails from './ProductDetails';
 import Loading from '@/components/Loading';
+import { client } from '@/lib/sanity';
 
-const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  useCdn: false,
-  apiVersion: '2023-05-03',
-});
 
 async function getProduct(slug: string) {
   return await client.fetch(
