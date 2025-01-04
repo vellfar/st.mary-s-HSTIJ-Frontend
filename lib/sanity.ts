4502bee7edf155870802d0e3e449fcb3e97cc214
vellfar <vellfarenterprises@gmail.com>
2025-01-04 10:11:00 +0300

    sanity

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,10 +1,18 @@
 import { createClient } from 'next-sanity'
 import imageUrlBuilder from '@sanity/image-url'
 
+if (!process.env.NEXT_PUBLIC_SANITY_PROJECT_ID) {
+  throw new Error('NEXT_PUBLIC_SANITY_PROJECT_ID environment variable is not set')
+}
+
+if (!process.env.NEXT_PUBLIC_SANITY_DATASET) {
+  throw new Error('NEXT_PUBLIC_SANITY_DATASET environment variable is not set')
+}
+
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: '2021-10-21',
+  apiVersion: '2023-05-03', // Use the latest API version
   useCdn: process.env.NODE_ENV === 'production',
   token: process.env.SANITY_API_TOKEN,
 })
