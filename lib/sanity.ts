55bac72e2b68c1759a415d685317bbb8fc934aa2
vellfar <vellfarenterprises@gmail.com>
2025-01-04 10:36:24 +0300

    variables

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,14 +1,6 @@
 import { createClient } from 'next-sanity'
 import imageUrlBuilder from '@sanity/image-url'
 
-if (!process.env.NEXT_PUBLIC_SANITY_PROJECT_ID) {
-  throw new Error('NEXT_PUBLIC_SANITY_PROJECT_ID environment variable is not set')
-}
-
-if (!process.env.NEXT_PUBLIC_SANITY_DATASET) {
-  throw new Error('NEXT_PUBLIC_SANITY_DATASET environment variable is not set')
-}
-
 export const client = createClient({
   projectId: "epzg1e40",
   dataset: "production",
