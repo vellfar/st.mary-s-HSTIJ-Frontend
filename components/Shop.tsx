13aaaa6b066f90ac6ba18024ed9aeaea2ece652d
vellfar <vellfarenterprises@gmail.com>
2025-01-05 22:51:35 +0300

    button

--- components/Shop.tsx
+++ components/Shop.tsx
@@ -100,7 +100,13 @@ export default function Shop({ initialProducts, initialCategories }: ShopSection
                 </Link>
               </Card>
           ))}
+          
         </motion.div>
+        <Link href="/shop" className='flex mt-10 justify-center items-center align-middle'>
+            <Button size="lg" className="bg-blue-600 text-white hover:bg-blue-700 transition-colors duration-300">
+              All Products
+            </Button>
+          </Link>
       </div>
     </section>
   )
